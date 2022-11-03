31ff4276d4c5d7d4dc44906bff73d3064f7c27d2
Ivan <ivan.lomaka5@gmail.com>
2022-11-03 22:31:26 +0100

    2 righe ci sta

--- conteggio-parola/conteggioSubstring.py
+++ conteggio-parola/conteggioSubstring.py
@@ -3,3 +3,6 @@ find = input("Bella, inserisci la parola da cercare: ") or 'perchè'
 for n in range(len(string)):
     if(string[n:(n + len(find))] == find): 
         print(str(n) + string[n:(n + len(find))])
+
+#string = input("Bella, inserisci una frase: ")
+#for n in range(len(string)): print((str(n) + '\n') if string[n:(n + len('perchè'))] == 'perchè' else "", end='')
